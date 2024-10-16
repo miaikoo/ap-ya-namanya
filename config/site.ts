c28b661fd7437afe4185e30099be8c806f5c6116
aqilazn <162773957+aqilazn@users.noreply.github.com>
2024-10-17 04:26:21 +0700

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
-  name: "ap-ya-namany",
+  name: "MAMdulu",
   url: "https://example.com",
   description: "Nextjs 14 blog using velite, tailwind and shadcn",
   author: "MAMdulu",
-  author1: "Aqila Zahira",
-  author2: "Hikmia Sofia",
-  author3: "Mazaya Khairana",
+  author1: "Aqila Zahira Naia Puteri Arifin",
+  author2: "Hikmia Sofia Nur Izzati",
+  author3: "Mazaya Khairana Nariswari",
   links: {
     twitter: "https://twitter.com/jollyshopland",
     github: "https://github.com/jolbol1",
