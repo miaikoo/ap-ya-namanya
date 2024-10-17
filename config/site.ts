d937922e166d726b2e2a51f0af5ae2049d36c9c7
aqilazn <aqilazhn05@gmail.com>
2024-10-17 07:08:49 +0700

    update

--- config/site.ts
+++ config/site.ts
@@ -1,16 +1,16 @@
 export const siteConfig = {
-  name: "MAMdulu",
-  url: "https://example.com",
-  description: "Nextjs 14 blog using velite, tailwind and shadcn",
-  author: "MAMdulu",
-  author1: "Aqila Zahira Naia Puteri Arifin",
-  author2: "Hikmia Sofia Nur Izzati",
-  author3: "Mazaya Khairana Nariswari",
-  links: {
-    twitter: "https://twitter.com/jollyshopland",
-    github: "https://github.com/jolbol1",
-    personalSite: "https://jamesshopland.com",
-  },
-};
-
-export type SiteConfig = typeof siteConfig;
+    name: "MAMdulu",
+    url: "https://example.com",
+    description: "Nextjs 14 blog using velite, tailwind and shadcn",
+    author: "MAMdulu",
+    author1: "Aqila Zahira Naia Puteri Arifin",
+    author2: "Hikmia Sofia Nur Izzati",
+    author3: "Mazaya Khairana Nariswari",
+    links: {
+      twitter: "https://twitter.com/jollyshopland",
+      github: "https://github.com/jolbol1",
+      personalSite: "https://jamesshopland.com",
+    },
+  };
+  
+  export type SiteConfig = typeof siteConfig;
